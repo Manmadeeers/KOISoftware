6b217b876581bd39de3c8fd65cd1e3a81668913a
Lietuvis hakeris <vaclovkacinskas@gmail.com>
2023-12-27 22:25:57 +0300

    Start of Tick

--- Project/Project/Header.h
+++ Project/Project/Header.h
@@ -6,6 +6,8 @@
 #include <ctime>
 #include <vector>
 #include <conio.h>
+#include <string.h>
 
 #include "TestYourKnowledge.h"
+#include "Tickets_and_Answers.h"
 using namespace std;
--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -3,6 +3,5 @@
 
 void main()
 {
-	setlocale(LC_ALL, "Russian");
-	
+	setlocale(LC_ALL, "Russian");	
 }
