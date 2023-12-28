ff15ef2f86786ddfbb0ceb730d421d26a872af7c
Manmad <philmade6@gmail.com>
2023-12-29 00:26:20 +0300

    site almost ready

---
+++ Site/egor.js
@@ -0,0 +1,7 @@
+let egor = document.getElementsByClassName("Korobov")[0];
+egor.addEventListener("mouseover",function(){
+    egor.src = "Korobov1.jpg";
+})
+egor.addEventListener("mouseout",function(){
+    egor.src = "Korobov.jpg";
+})
